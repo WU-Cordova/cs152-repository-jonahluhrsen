3df501d80783b080d6e82d233d60487e966a18ce
jonahluhrsen <jjluhrsen@willamette.edu>
2025-02-21 12:57:27 -0800

    Finished Array

--- datastructures/array.py
+++ datastructures/array.py
@@ -11,80 +11,107 @@ import os
 from typing import Any, Iterator, overload
 import numpy as np
 from numpy.typing import NDArray
-
-
 from datastructures.iarray import IArray, T
-
 
 class Array(IArray[T]):  
 
     def __init__(self, starting_sequence: Sequence[T]=[], data_type: type=object) -> None: 
-        self.__logical_size: int = len(starting_sequence)
-        self.__physcial_size: int = self.__logical_size
-        self.__data_type: int = data_type
-
         if not isinstance(starting_sequence, Sequence):
-            raise ValueError('starting_sequence must be a valid sequence type')
-        
-        for index in range(self.__logical_size):
-            if not isinstance(starting_sequence[index], self.__data_type):
-                raise TypeError('Items in starting are not all the same type')
+            raise ValueError("starting_sequence must be a valid sequence type.")
+        if not isinstance(data_type, type):
+            raise TypeError("data_type must be a valid type.")
+        if not isinstance (starting_sequence[0], data_type):
+            raise TypeError("Need to pass in same data types")
+        self.__elements = np.array(starting_sequence, dtype= data_type)
+        self.__logical = len(starting_sequence)
+        self.__physical = len(self.__elements)
+        self.__data_type = data_type if data_type != object else type(starting_sequence[0])
 
-        self._items: NDArray = np.empty(self.__logical_size, dtype=self.__data_type)
-
-        for index in range(self.__logical_size):
-            self.__items[index] = starting_sequence[index]
-
-    @overload
-    def __getitem__(self, index: int) -> T: ...
-    @overload
-    def __getitem__(self, index: slice) -> Sequence[T]: ...
     def __getitem__(self, index: int | slice) -> T | Sequence[T]:
-            raise NotImplementedError('Indexing not implemented.')
+            if  not isinstance(index, slice) and not isinstance(index, int):
+                raise TypeError("Needs to be int or slice")
+            if isinstance(index, slice):
+                start, stop = index.start, index.stop
+                if start >= self.__logical or stop > self.__logical:
+                    raise IndexError('Not in Bounds')
+                items_to_return = self.__elements[index].tolist()
+                return Array(starting_sequence= items_to_return, data_type = self.__data_type) 
+            return self.__elements[index]
     
     def __setitem__(self, index: int, item: T) -> None:
-        raise NotImplementedError('Indexing not implemented.')
+        if type(item) != self.__data_type:
+            raise TypeError("Item you're setting isn't the same type as the array")
+        if index > self.__logical:
+            raise IndexError("Index is out of bounds")
+        self.__elements[index] = item
 
     def append(self, data: T) -> None:
-        raise NotImplementedError('Append not implemented.')
+        if type(data) != self.__data_type:
+            raise ValueError("Data is the wrong type")
+        if self.__logical == self.__physical:
+            self.__physical *= 2
+            self.__elements.resize(self.__physical)  
+        self.__logical += 1
+        self.__elements[self.__logical] = data
 
     def append_front(self, data: T) -> None:
-        raise NotImplementedError('Append front not implemented.')
+        if type(data) != self.__data_type:
+            raise ValueError("Data is the wrong type")
+        if self.__logical == self.__physical:
+            self.__physical *= 2
+            self.__elements.resize(self.__physical)
+        for i in range(self.__logical -1, 0, -1):
+            self.__elements[i +1] = self.__elements[i]
+        self.__elements[0] = data
+        self.__logical += 1
 
     def pop(self) -> None:
-        raise NotImplementedError('Pop not implemented.')
+        del self[self.__logical-1]
     
     def pop_front(self) -> None:
-        raise NotImplementedError('Pop front not implemented.')
+        del self[self.__logical[0]]
 
     def __len__(self) -> int: 
-        raise NotImplementedError('Length not implemented.')
+        return self.__logical
 
     def __eq__(self, other: object) -> bool:
-        raise NotImplementedError('Equality not implemented.')
+        if isinstance(other, Array):
+            if self.__data_type == other.__data_type and (self.__elements == other.__elements).all() and self.__logical == other.__logical and self.__physical == other.__physical:
+                return True
+        return False
     
+    def __ne__(self, other):
+        return not self==other
+
     def __iter__(self) -> Iterator[T]:
-        raise NotImplementedError('Iteration not implemented.')
+        return iter(self.__elements[:self.__logical])
 
     def __reversed__(self) -> Iterator[T]:
-        raise
+        reversed_elements = self.__elements[self.__logical-1::-1]
+        return iter(reversed_elements)
 
     def __delitem__(self, index: int) -> None:
-       raise NotImplementedError('Delete not implemented.')
+        for i in range(index, self.__logical -1):
+            self.__elements[i] = self.__elements[i + 1]
+        self.__logical -= 1
+        if self.__logical <= self.__physical/4:
+            self.__physical/=2
+            self.__elements.resize(self.__physical)
 
     def __contains__(self, item: Any) -> bool:
-        raise NotImplementedError('Contains not implemented.')
+        return item in self.__elements[:self.__logical]
 
     def clear(self) -> None:
-        raise NotImplementedError('Clear not implemented.')
+        self.__elements = np.array([])
+        self.__logical = 0
+        self.__physical = 0
 
     def __str__(self) -> str:
         return '[' + ', '.join(str(item) for item in self) + ']'
     
     def __repr__(self) -> str:
-        return f'Array {self.__str__()}, Logical: {self.__item_count}, Physical: {len(self.__items)}, type: {self.__data_type}'
+        return f'Array {self.__str__()}, Logical: {self.__logical}, Physical: {self.__physical}, type: {self.__data_type}'
     
-
 if __name__ == '__main__':
     filename = os.path.basename(__file__)
     print(f'This is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
